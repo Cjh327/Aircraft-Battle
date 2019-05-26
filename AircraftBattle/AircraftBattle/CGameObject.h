c1dd37410bada442d84faa566209bf9e04536030
CjhGm <cjh327@126.com>
2019-05-26 16:20:41 +0800

    add bmp

--- AircraftBattle/AircraftBattle/CGameObject.h
+++ AircraftBattle/AircraftBattle/CGameObject.h
@@ -6,5 +6,7 @@ protected:
 	CPoint point;
 public:
 	CGameObject(int x, int y);
+	//加载该游戏对象对应的图像,图像对象是共有数据，只需要加载一份，使用静态方法
+	static BOOL loadImage(CImageList& imageList, UINT bmpID, COLORREF crMask, int cx, int cy, int nInitial);
 };
 
--- AircraftBattle/AircraftBattle/Resource.h
+++ AircraftBattle/AircraftBattle/Resource.h
@@ -8,12 +8,14 @@
 #define IDD_AIRCRAFTBATTLE_DIALOG       102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
+#define IDB_BITMAP_ME                   134
+#define IDB_BITMAP_ME1                  136
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
